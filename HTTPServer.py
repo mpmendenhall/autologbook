7b0bae3b2951f4fb88d726439080fb66752515f9
Michael Mendenhall <mendenhall5@llnl.gov>
2017-10-03 11:45:29 -0700

    Add basic password authentication option

--- HTTPServer.py
+++ HTTPServer.py
@@ -5,11 +5,31 @@ import http.server
 import os
 from optparse import OptionParser
 import ssl
+import base64
 
-class LoggerCGIHander(http.server.CGIHTTPRequestHandler):
+class PWAuthHandler(http.server.CGIHTTPRequestHandler):
+    def checkAuthentication(self):
+        if self.server.auth is None: return True
+        auth = self.headers.get('Authorization')
+        if auth != "Basic %s" % self.server.auth:
+            self.send_response(401)
+            self.send_header("WWW-Authenticate", 'Basic realm="autologbook"')
+            self.end_headers();
+            return False
+        return True
+
+    def do_GET(self):
+        if not self.checkAuthentication(): return
+        super().do_GET()
+
+    def do_POST(self):
+        if not self.checkAuthentication(): return
+        super().do_POST()
+
+class LoggerCGIHander(PWAuthHandler):
     cgi_directories = ['/cgi-logger']
 
-class ConfigCGIHandler(http.server.CGIHTTPRequestHandler):
+class ConfigCGIHandler(PWAuthHandler):
     cgi_directories = ['/cgi-config', '/cgi-logger']
 
 parser = OptionParser()
@@ -18,6 +38,7 @@ parser.add_option("--host", default="localhost", help="server host")
 parser.add_option("--mode", default="logger", help="server mode: logger or config")
 parser.add_option("--db",   help="path to database")
 parser.add_option("--dir",  help="base directory for served content")
+parser.add_option("--pwd",  help="user:password HTTP Basic Authentication")
 parser.add_option("--cert", help="https certificate .pem file")
 parser.add_option("--key",  help="https certifical key file")
 options, args = parser.parse_args()
@@ -28,10 +49,17 @@ if options.db: os.environ["CONFIGWEBMANAGER_DB"] = options.db
 
 if  options.mode == "config":
     print("Webserver for autologbook Configuration DB")
-    httpd = http.server.HTTPServer((options.host, options.port), ConfigCGIHandler)
+    handler = ConfigCGIHandler
 else:
     print("Webserver for autologbook Logger DB")
-    httpd = http.server.HTTPServer((options.host, options.port), LoggerCGIHander)
+    handler = LoggerCGIHander
+
+httpd = http.server.HTTPServer((options.host, options.port), handler)
+
+if options.pwd:
+    httpd.auth = base64.b64encode(options.pwd.encode()).decode()
+else:
+    httpd.auth = None    
 
 # openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -days 365 -subj "/C=US/ST=California/L=Livermore/O=Company Name/OU=Org/CN=www.example.com" -nodes
 if options.cert:
