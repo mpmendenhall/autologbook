b6806281f33fe1cc14cba5d1be29571136612926
Michael Mendenhall <michael@mendenhall.science>
2020-08-08 20:17:34 -0700

    Monitoring gpsd support

--- scripts/EnvironmentalMonitor.py
+++ scripts/EnvironmentalMonitor.py
@@ -8,6 +8,7 @@ import time
 import board
 import busio
 import adafruit_bmp3xx
+from gps import *
 
 def h2P(h): return 1013.25 * (1 - 2.25577e-5 * h)**5.25588
 def P2h(P): return (1 - (P/1013.25)**(1./5.25588))/2.25577e-5
@@ -22,23 +23,61 @@ with xmlrpc.client.ServerProxy('http://%s:%i'%(log_xmlrpc_host, log_xmlrpc_write
     bmp3xx_T = DBL.create_readout("T", "BMP388", "ambient temperature", "deg. C")
     bmp3xx_P = DBL.create_readout("P", "BMP388", "ambient pressure", "mbar")
 
-def read_sensors():
+    gps_group = DBL.create_readgroup("GPS", "Global Positioning System receiver")
+    gps_lat = DBL.create_readout("lat", "GPS", "Latitude", "degrees N")
+    gps_lon = DBL.create_readout("lon", "GPS", "Longitude", "degrees E")
+    gps_alt = DBL.create_readout("alt", "GPS", "Altitude", "m")
+
+def read_sensors(i):
     DBL = xmlrpc.client.ServerProxy('http://%s:%i'%(log_xmlrpc_host, log_xmlrpc_writeport), allow_none=True)
     i2c = busio.I2C(board.SCL, board.SDA)
+
+    t = time.time()
 
     bmp3xx = adafruit_bmp3xx.BMP3XX_I2C(i2c)
     bmp3xx.pressure_oversampling = 32
     bmp3xx.temperature_oversampling = 32
-    t = time.time()
     T = bmp3xx.temperature
     P = bmp3xx.pressure
     DBL.log_readout(bmp3xx_T, T, t)
     DBL.log_readout(bmp3xx_P, P, t)
 
+    if not i%6:
+        gpsd = gps(mode=WATCH_ENABLE|WATCH_NEWSTYLE)
+
+        report = gpsd.next()
+        retries = 6
+        while report['class'] != 'TPV' and retries:
+            print(report['class'])
+            report = gpsd.next()
+            retries -= 1
+
+        if report['class'] == 'TPV':
+            tstr = getattr(report,'time',None)
+
+            lat = getattr(report,'lat', None)
+            if lat is not None: DBL.log_readout(gps_lat, lat, t)
+            lon = getattr(report,'lon', None)
+            if lon is not None: DBL.log_readout(gps_lon, lon, t)
+            alt = getattr(report,'alt', None)
+            if alt is not None: DBL.log_readout(gps_alt, alt, t)
+            print(tstr,lat,lon,alt)
+
+            #
+            #print  getattr(report,'epv','nan'),"\t",
+            #print  getattr(report,'ept','nan'),"\t",
+            #print  getattr(report,'speed','nan'),"\t",
+            #print getattr(report,'climb','nan'),"\t"
+
     DBL.commit()
 
+ii = 0
 while True:
-    print("\nEnvironmental sensors readout");
-    try: read_sensors()
-    except: traceback.print_exc()
+    print("\nEnvironmental sensors readout", ii);
+    try:
+        read_sensors(ii)
+        ii += 1
+    except:
+        ii = 0
+        traceback.print_exc()
     time.sleep(10)
