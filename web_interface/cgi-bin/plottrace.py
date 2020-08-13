ca7feb304ab671287839de4452428c44523ca3fc
Michael Mendenhall <michael@mendenhall.science>
2020-08-13 14:06:34 -0700

    Link-based single-page plots

--- web_interface/cgi-bin/plottrace.py
+++ web_interface/cgi-bin/plottrace.py
@@ -9,6 +9,7 @@ import zlib
 import cgi
 import time
 from datetime import datetime
+import os
 import sys
 
 class TracePlotter(PlotMaker):
@@ -44,26 +45,14 @@ class TracePlotter(PlotMaker):
                 self.readings[rid] = pickle.loads(zlib.decompress(s.datapoints_compressed(rid, self.tm, self.t0, 150).data))[::-1]
                 self.ids.append(rid)
 
-    def makePage(self, img = None):
-        if not self.readings:
-            if img:
-                sys.stdout.buffer.write(b'Content-Type: image/svg+xml\nContent-Encoding: gzip\n\n')
-                sys.stdout.buffer.write(open("logo.svgz", "rb").read())
-                return
-            P,b = makePageStructure("plotter fail!")
-            addTag(b,"h1",contents="no plots found!")
-            print(docHeaderString())
-            print(prettystring(P))
-            return
+    def dumpImage(self, img):
+        """Generate response page"""
 
-        # single or multiple plots?
-        ptitle = "plottrace"
-        subtitle = "readings as of %s"%time.asctime()
-        chn = None
-        if len(self.readings) == 1:
-            chn = self.channels[self.ids[0]]
-            ptitle = "%s plot"%chn["name"]
-            subtitle = "%s %s"%(chn["name"], subtitle)
+        # no data to plot?
+        if not self.readings:
+            sys.stdout.buffer.write(b'Content-Type: image/svg+xml\nContent-Encoding: gzip\n\n')
+            sys.stdout.buffer.write(open("logo.svgz", "rb").read())
+            return
 
         self.renames = dict([(c,self.channels[c]["name"].replace("_"," ")) for c in self.channels])
 
@@ -84,22 +73,49 @@ class TracePlotter(PlotMaker):
             if not self.xtime: self.x_txs[r] = (lambda x, t0=self.t0+self.dt0: (x-t0)/self.tscale)
             else: self.x_txs[r] = (lambda x, dx=(datetime.now()-datetime.utcnow()).total_seconds(): x+dx)
 
-        if img:
-            if img not in ["pdf","svg","txt","png","gif","svg"]: img = "svgz"
-            self.make_dump(img, self.ids, "set xtics rotate by 35 right offset 0,-0.2\n" if self.xtime else "")
-        else:
-            pstr = self.make_svg(self.ids, "set xtics rotate by 35 right offset 0,-0.2\n" if self.xtime else "")
-            P,b = makePageStructure(ptitle, refresh=300)
-            addTag(b,"h1",contents=[subtitle, makeLink("/index.html","[Home]")])
-            g = ET.Element('figure', {"style":"display:inline-block"})
-            if chn: addTag(g,"figcaption",contents=chn["descrip"])
-            if pstr: addTag(g, "div", {"class":"lightbg"}, contents=ET.fromstring(pstr))
-            b.append(g)
-            print(docHeaderString(), unmangle_xlink_namespace(prettystring(P)))
+        if img not in ["pdf","svg","txt","png","gif","svg"]: img = "svgz"
+        self.make_dump(img, self.ids, "set xtics rotate by 35 right offset 0,-0.2\n" if self.xtime else "")
+
+def makePage(form):
+    """Single-plot webpage"""
+
+    # scan readouts metadata
+    s = xmlrpc.client.ServerProxy('http://%s:%i'%(log_DB_host,log_xmlrpc_port), allow_none=True)
+    channels = set()
+    for rid in form.getlist("rid"):
+        try: rid = int(rid)
+        except: continue
+        if rid in channels: return
+        ri = s.readout_info(rid)
+        if ri:
+            ptitle = ri[0]+" plot"
+            pheader = ri[0] + " readings as of %s"%time.asctime()
+            subtitle = ri[0] + ": " + ri[1]
+            channels.add(rid)
+
+    # multi-trace plots
+    if len(channels) != 1:
+        ptitle = "plottrace"
+        pheader = "Readings as of %s"%time.asctime()
+        subtitle = ""
+
+    args = os.environ.get('QUERY_STRING', '')
+
+    P,b = makePageStructure(ptitle, refresh=300)
+    addTag(b,"h1",contents=[pheader, makeLink("/index.html","[Home]")])
+    f = addTag(b, 'figure', {"style":"display:inline-block"})
+    addTag(f, 'img', {"class":"lightbg", "width":"600", "height":"480", "src":"/cgi-bin/plottrace.py?" + args + "&img=y"})
+    addTag(f, 'figcaption', {}, [subtitle, makeLink("/cgi-bin/plottrace.py?" + args + "&img=pdf", "[PDF]")])
+    print(docHeaderString(), unmangle_xlink_namespace(prettystring(P)))
 
 if __name__=="__main__":
 
     form = cgi.FieldStorage()
+    img = form.getvalue("img",None)
+    if not img:
+        makePage(form)
+        exit(0)
+
     try:
         dt = abs(float(form.getvalue("dt","12")))
         if not dt < 31*24: dt = 31*24
@@ -114,5 +130,4 @@ if __name__=="__main__":
     except: pass
     tp.get_readings(form.getlist("rid"))
     tp.keypos = form.getvalue("key","top left")
-
-    tp.makePage(img = form.getvalue("img",None))
+    tp.dumpImage(img)
